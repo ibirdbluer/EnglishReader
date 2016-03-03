1286dec91347cd2694165337bc3162d89efbf28e
qianyu <qianyu@ihefe.com>
2016-03-03 14:43:06 +0800

    show content

--- EnglishReader/ViewController.swift
+++ EnglishReader/ViewController.swift
@@ -8,16 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var theScrollView: UIScrollView!
+    
+    // Mark: - life circle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add locally formatted strings to paragraph
+        let paraStyle = NSMutableParagraphStyle()
+        paraStyle.firstLineHeadIndent = 15.0
+        paraStyle.paragraphSpacingBefore = 10.0
+        paraStyle.lineSpacing = 20.0
+        
+        let titleAttributes = [NSFontAttributeName: UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline), NSForegroundColorAttributeName: UIColor.purpleColor(), NSParagraphStyleAttributeName: paraStyle]
+        
+
+        
+        // Apply paragraph styles to paragraph
+//        para.addAttribute(NSParagraphStyleAttributeName, value: paraStyle, range: NSRange(location: 0,length: para.length))
+        
+        
+//        let titleString = NSAttributedString(string: "Read all about it!", attributes: titleAttributes)
+
+        let fileLocation = NSBundle.mainBundle().pathForResource("text", ofType: "txt")!
+        let text : String
+        do
+        {
+            text = try String(contentsOfFile: fileLocation)
+        }
+        catch
+        {
+            text = ""
+        }
+        
+        
+        let storage = NSTextStorage(string: text, attributes: titleAttributes)
+        let layoutManager = NSLayoutManager()
+        storage.addLayoutManager(layoutManager)
+        
+        var i = 0
+        while true {
+            let width = CGFloat(Int(self.view.frame.size.width) * i)
+            let textContainer = NSTextContainer(size: CGSizeMake(self.view.frame.size.width, self.view.frame.size.height - 20))
+            layoutManager.addTextContainer(textContainer)
+            
+            let theTextView = UITextView(frame: CGRectMake(width, 0, self.view.frame.size.width, self.view.frame.size.height), textContainer: textContainer)
+            theTextView.delegate = self
+//            theTextView.editable = true
+            self.theScrollView.addSubview(theTextView)
+            i++
+            
+            let range = layoutManager.glyphRangeForTextContainer(textContainer)
+            if range.length + range.location == text.characters.count {
+                break
+            }
+            
+        }
+        
+        self.theScrollView.contentSize = CGSizeMake(self.view.frame.size.width * CGFloat(i), self.view.frame.size.height)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // Mark: - scroll view delegate
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x, 0)
     }
 
 
